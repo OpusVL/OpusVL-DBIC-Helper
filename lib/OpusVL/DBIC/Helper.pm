61bf776059c6dbaed1106b53489355bce128a0f4
Colin Newell <colin@opusvl.com>
2015-11-10 10:58:10 +0000

    Initial commit

---
+++ lib/OpusVL/DBIC/Helper.pm
@@ -0,0 +1,34 @@
+use strict;
+use warnings;
+package OpusVL::DBIC::Helper;
+
+our $VERSION = '0.001';
+
+1;
+
+=head1 NAME
+
+OpusVL::DBIC::Helper
+
+=head1 DESCRIPTION
+
+This module provides helper classes/roles for use with DBIC.
+
+=over
+
+=item * L<OpusVL::DBIC::Helper::RolesFor::Schema::DataInitialisation>
+
+=back
+
+=head1 METHODS
+
+=head1 ATTRIBUTES
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2015 OpusVL.
+
+This software is licensed according to the "IP Assignment Schedule" provided with the development project.
+
+=cut
